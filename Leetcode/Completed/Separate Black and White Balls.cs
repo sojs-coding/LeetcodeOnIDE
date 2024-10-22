11fc8e4ddb86ce51326f64f07ed4c67045e348d5
sojs-coding <sojscodes@gmail.com>
2024-10-22 14:01:01 +0800

    Create Separate Black and White Balls.cs

---
+++ Leetcode/Completed/Separate Black and White Balls.cs
@@ -0,0 +1,40 @@
+﻿namespace Leetcode;
+
+public class Separate_Black_and_White_Balls : ILeetcodeSolution
+{
+    public void Run()
+    {
+        Solution solution = new Solution();
+        string s = "101";
+        long result;
+        result = solution.MinimumSteps(s);
+        Console.WriteLine($"{s} : {result}");
+        s = "100";
+        result = solution.MinimumSteps(s);
+        Console.WriteLine($"{s} : {result}");
+        s = "0111";
+        result = solution.MinimumSteps(s);
+        Console.WriteLine($"{s} : {result}");
+    }
+    
+    public class Solution {
+        public long MinimumSteps(string s)
+        {
+            long numberOfBlackBallsBeforeWhite = 0;
+            long totalSwaps = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].Equals('1')) // Black ball
+                {
+                    numberOfBlackBallsBeforeWhite += 1;
+                }
+                else // White ball
+                {
+                    totalSwaps += numberOfBlackBallsBeforeWhite;
+                }
+            }
+
+            return totalSwaps;
+        }
+    }
+}

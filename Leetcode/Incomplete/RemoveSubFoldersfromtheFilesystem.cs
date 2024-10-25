bd3c76f8a3f210719d291e09e2734f3cc106add6
sojs-coding <sojscodes@gmail.com>
2024-10-25 10:45:47 +0800

    Create RemoveSubFoldersfromtheFilesystem.cs

---
+++ Leetcode/Incomplete/RemoveSubFoldersfromtheFilesystem.cs
@@ -0,0 +1,36 @@
+﻿namespace Leetcode;
+
+public class RemoveSubFoldersfromtheFilesystem : ILeetcodeSolution
+{
+    public void Run()
+    {
+        Solution solution = new Solution();
+        IList<string> output;
+        IList<string> result;
+        string[] folder;
+        
+        folder = ["/a","/a/b","/c/d","/c/d/e","/c/f"];
+        output = ["/a", "/c/d", "/c/f"];
+        result = solution.RemoveSubfolders(folder);
+        solution.CompareResult(output, result);
+
+        folder = ["/a", "/a/b/c", "/a/b/d"];
+        output = ["/a"];
+        result = solution.RemoveSubfolders(folder);
+        solution.CompareResult(output, result);
+
+        folder = ["/a/b/c", "/a/b/ca", "/a/b/d"];
+        output = ["/a/b/c", "/a/b/ca", "/a/b/d"];
+        result = solution.RemoveSubfolders(folder);
+        solution.CompareResult(output, result);
+    }
+    
+    public class Solution : LeetcodeSolution {
+        public IList<string> RemoveSubfolders(string[] folder)
+        {
+            /*
+             
+             */
+        }
+    }
+}

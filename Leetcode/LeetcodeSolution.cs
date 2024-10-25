65a610fbf3855c3b31911421d2680c868039418c
sojs-coding <sojscodes@gmail.com>
2024-10-25 13:18:52 +0800

    CompareResult via generic methods

--- Leetcode/LeetcodeSolution.cs
+++ Leetcode/LeetcodeSolution.cs
@@ -1,13 +1,50 @@
-﻿namespace Leetcode;
+﻿using System.Collections;
+
+namespace Leetcode;
 
 public abstract class LeetcodeSolution
 {
-    public bool CompareResult(object answer, object result)
+    private bool CompareResult<T>(T answer, T result)
     {
+        if (answer is IList answerList && result is IList resultList)
+        {
+            return CompareLists(answerList, resultList);
+        }
+        
         return answer.Equals(result);
     }
+    
+    private bool CompareLists(IList answer, IList result)
+    {
+        if (answer.Count != result.Count)
+        {
+            return false;
+        }
 
-    public void PrintResult(object answer, object result)
+        for (int i = 0; i < answer.Count; i++)
+        {
+            if (!answer[i].Equals(result[i]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void PrintResult<T>(T answer, T result)
+    {
+        if (CompareResult(answer, result))
+        {
+            Console.WriteLine("Passed");
+        }
+        else
+        {
+            Console.WriteLine("Failed");
+        }
+    }
+    
+    public void PrintResult(IList answer, IList result)
     {
         if (CompareResult(answer, result))
         {

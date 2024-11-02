564faaa537abae6eea8ae3679b6c0791f4664da6
sojs-coding <sojscodes@gmail.com>
2024-11-02 17:20:53 +0800

    Create IsCircularSentence.cs

---
+++ Leetcode/Completed/IsCircularSentence.cs
@@ -0,0 +1,55 @@
+﻿namespace Leetcode;
+
+public class IsCircularSentence : ILeetcodeSolution
+{
+    public void Run()
+    {
+        Solution solution = new Solution();
+        string sentence;
+        bool result;
+        bool answer;
+
+        sentence = "leetcode exercises sound delightful";
+        answer = true;
+        result = solution.IsCircularSentence(sentence);
+        solution.PrintResult(answer, result);
+
+        sentence = "eetcode";
+        answer = true;
+        result = solution.IsCircularSentence(sentence);
+        solution.PrintResult(answer, result);
+
+        sentence = "Leetcode is cool";
+        answer = false;
+        result = solution.IsCircularSentence(sentence);
+        solution.PrintResult(answer, result);
+    }
+    
+    public class Solution : LeetcodeSolution {
+        public bool IsCircularSentence(string sentence)
+        {
+            string[] words;
+            words = sentence.Split(' ');
+            char back = words[0].Last();
+            char front;
+            for (int i = 1; i < words.Length; i++)
+            {
+                front = words[i].First();
+                if (!front.Equals(back))
+                {
+                    return false;
+                }
+
+                back = words[i].Last();
+            }
+
+            front = words[0].First();
+            if (!back.Equals(front))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
